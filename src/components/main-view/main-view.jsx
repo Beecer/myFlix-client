04908441324c89f85bb6fc64be2108140aaabeca
Beecer <94152383+Beecer@users.noreply.github.com>
2022-03-03 16:13:51 -0800

    installed axios in main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,41 +1,30 @@
 import React from "react";
+import axios from "axios";  
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from "../movie-view/movie-view";
-
-let imgPath='./img/'
 
 export class MainView extends React.Component {
 
   constructor() {
     super();
       this.state ={
-        movies: [
-          { _id: 1,
-           Title: 'Inception',
-           Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-           ImagePath: <div src='img\inception.jpg' />,
-           Genre: 'Action',
-           Director: 'Christopher Nolan'
-          },
-          { _id: 2, 
-            Title: 'The Shawshank Redemption', 
-            Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
-            ImagePath: 'img\Shawshank.jpg',
-            Genre: 'Drama',
-            Director: 'Frank Darabont'
-          },
-        { _id: 3, 
-          Title: 'Gladiator', 
-          Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
-          ImagePath: 'img\Gladiator.jpg',
-          Genre: 'Drama',
-          Director: 'Ridley Scott'
-        }
-        ],
+        movies: [],
         selectedMovie: null
       };
     }
+
+  componentDidMount() {
+    axios.get('https://mymoviesapp775.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
   
   setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -46,7 +35,7 @@ export class MainView extends React.Component {
   render() {
     const {movies, selectedMovie} = this.state;
   
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
   
     return (
       <div className="main-view">
