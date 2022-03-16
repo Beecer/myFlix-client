cc12ddbfc6c548c016e5ed98a9947bb3f191107b
Beecer <94152383+Beecer@users.noreply.github.com>
2022-03-15 18:00:37 -0700

    added CardGroup

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap';
 
 import './movie-card.scss';
 import { CardGroup } from "react-bootstrap";
@@ -13,11 +14,10 @@ export class MovieCard extends React.Component {
     return ( 
       <CardGroup>
       <Card className="bg-light text-black" border='danger' style={{ width: '14rem', height: '15rem',margin: '.5rem' }}>
-        <Card.Img variant="top" src={"movie.ImagePath"} />
         <Card.Body>
+          <Card.Img  src={movie.ImagePath} />
           <Card.Title>{movie.Title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{movie.Genre.Year}</Card.Subtitle>
-          <Card.Text>{movie.Director.Name}</Card.Text>
+          <Card.Subtitle className="mb-2 text-muted">{movie.Year}</Card.Subtitle>
           <Button onClick={() => onMovieClick(movie)} variant="outline-dark">Details</Button>
         </Card.Body>
       </Card>
