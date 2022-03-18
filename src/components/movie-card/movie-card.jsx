30fde5696a98f633c6a703712679ebea60669ce3
Beecer <94152383+Beecer@users.noreply.github.com>
2022-03-18 16:22:27 -0700

    add movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,27 @@
 import React from "react";  
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+import './movie-card.scss';
+import { CardGroup } from "react-bootstrap";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick} = this.props;
 
-    return ( <div onClick={() => onMovieClick(movie) } className="movie-card">{movie.Title}
-    </div>
+    return ( 
+      <CardGroup>
+      <Card className="bg-light text-black" border='danger' style={{ width: '14rem', height: '15rem',margin: '.5rem' }}>
+      <Card.Img  variant="top" src={`${movie.ImagePath}`} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{movie.Year}</Card.Subtitle>
+          <Button onClick={() => onMovieClick(movie)} variant="outline-dark">Details</Button>
+        </Card.Body>
+      </Card>
+      </CardGroup>
     );
   }
 }
