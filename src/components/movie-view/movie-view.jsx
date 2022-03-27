3efde4a9c69867f34a8c460110a00ccec4fa5147
Beecer <94152383+Beecer@users.noreply.github.com>
2022-03-26 19:50:36 -0700

    fix errors

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
-
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import './movie-view.scss';
@@ -23,7 +23,7 @@ export class MovieView extends React.Component {
     const {movie, onBackClick} = this.props;
 
     return (
-      <div className="movie-view bg-light text-black" >
+      <div className="movie-view bg-light text-black" style={{marginTop: 150}} >
         <div className="movie-poster">
           <img src={movie.ImagePath} crossOrigin="true"/>
         </div>
@@ -37,16 +37,14 @@ export class MovieView extends React.Component {
         </div>
         <div className="movie-genre">
           <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-          <Link to={`/genres/${movie.Genre.Description}`}>
-          <Button variant="link">Details</Button>
+          <Link to={`/genre/${movie.Genre.Name}`}>
+            <Button variant='link'>{movie.Genre.Name}</Button>
           </Link>
         </div>
         <div className="movie-director">
           <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-          <Link to={`/directors/${movie.Director.Name}`}>
-          <Button variant="link">About</Button>
+          <Link to={`/director/${movie.Director.Name}`}>
+            <Button variant='link' >{movie.Director.Name}</Button>
           </Link>
         </div>
         <button onClick={() => {onBackClick(null); }} variant="outline-dark">Back</button>
@@ -61,11 +59,10 @@ MovieView.propTypes = {
     Description: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-  }).isRequired,
-  Director:PropTypes.shape({
-    Name: PropTypes.string.isRequired,
-  })
+    }).isRequired,
+    Director:PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    })
 }).isRequired,
   onBackClick: PropTypes.func.isRequired
 };
