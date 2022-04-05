ac17037048a8a10ae083dcebfc6b1b4fb93010a4
Beecer <94152383+Beecer@users.noreply.github.com>
2022-04-04 19:10:35 -0700

    add form

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,32 +1,48 @@
 import React from "react";
+import { Form, Button, Col, Row, Card } from "react-bootstrap";
 
-function UpdateUser(handleSubmit, handleUpdate){
+export function UpdateUser(props){
+  const user = props.userdata
+  const { handleSubmit, handleUpdate} = props;
+  
   return (
-    <form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
-      <h2>Want to change some Info?</h2>
-      <label>Username:</label>
-      <input
-        type='text'
-        name='Username'
-        defaultValue={user.Username}
-        onChange={e => handleUpdate(e)} />
-      <label>Password</label>
-      <input
-        type='password'
-        name='password'
-        defaultValue={user.Password}
-        onChange={e => handleUpdate(e)} />
-      <label>Email address</label>
-      <input
-        type='email'
-        name='email'
-        defaultValue={user.Email}
-        onChange={e => handleUpdate(e.target.value)} />
-      <button variant='primary' type='submit'>
-        Update
-        </button> 
-      </form>
+    <>
+    
+      <Form className="profile-form" border='danger' style={{marginTop: 50, marginBottom: 50}}>
+      <h5>Update Profile</h5>
+    
+        <Form.Group controlId="formUsername" className="mb-3">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+           type='text'
+           name='Username'
+           defaultValue={user.Username}
+           onChange={e => handleUpdate(e)} />
+        </Form.Group>
+       
+        <Form.Group controlId="formPassword" className="mb-3">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+           type='text'
+           name='Password'
+           defaultValue={user.Password}
+           onChange={e => handleUpdate(e)} />
+        </Form.Group>
+
+        <Form.Group controlId="formEmail" className="mb-3">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+           type='text'
+           name='Email'
+           defaultValue={user.Email}
+           onChange={e => handleUpdate(e)} />
+        </Form.Group>
+      
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+    </Form> 
+    </>
   )
 }
 
-export default UpdateUser;
