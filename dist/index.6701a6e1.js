12ddacda841211a0f6e98369c7c2f051991caa41
Beecer <94152383+Beecer@users.noreply.github.com>
2022-04-04 19:09:12 -0700

    update dist

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -24809,7 +24809,12 @@ function LoginView(props) {
             __self: this,
             children: /*#__PURE__*/ _jsxRuntime.jsx(_reactBootstrap.Card, {
                 className: "bg-light text-black",
-                c: true,
+                border: "danger",
+                style: {
+                    marginTop: 100,
+                    marginBottom: 50,
+                    borderRadius: 20
+                },
                 __source: {
                     fileName: "src/components/login-view/login-view.jsx",
                     lineNumber: 60
