c00237d0d0a61df023a634229191215dfb43d54c
Beecer <94152383+Beecer@users.noreply.github.com>
2022-04-09 17:16:27 -0700

    Update update-user.jsx

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Form, Button, Col, Row, Card } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
-export function UpdateUser(props){
+export function UpdatedUser(props){
   const user = props.userdata
   const { handleSubmit, handleUpdate} = props;
   
   return (
     <>
     
-      <Form className="profile-form" border='danger' style={{marginTop: 50, marginBottom: 50}}>
+      <Form className="profile-form" onSubmit={(e) =>handleSubmit(e)} border='danger' style={{marginTop: 50, marginBottom: 50}}>
       <h5>Update Profile</h5>
     
         <Form.Group controlId="formUsername" className="mb-3">
@@ -17,7 +17,8 @@ export function UpdateUser(props){
            type='text'
            name='Username'
            defaultValue={user.Username}
-           onChange={e => handleUpdate(e)} />
+           onChange={e => handleUpdate(e)} 
+           />
         </Form.Group>
        
         <Form.Group controlId="formPassword" className="mb-3">
@@ -26,7 +27,8 @@ export function UpdateUser(props){
            type='text'
            name='Password'
            defaultValue={user.Password}
-           onChange={e => handleUpdate(e)} />
+           onChange={e => handleUpdate(e)} 
+           />
         </Form.Group>
 
         <Form.Group controlId="formEmail" className="mb-3">
@@ -35,7 +37,8 @@ export function UpdateUser(props){
            type='text'
            name='Email'
            defaultValue={user.Email}
-           onChange={e => handleUpdate(e)}/>
+           onChange={e => handleUpdate(e)}           
+           />
         </Form.Group>
       
         <Button variant="primary" type="submit" onClick={handleSubmit}>
